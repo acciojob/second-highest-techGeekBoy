8981a314a46b20329c9a471639301d3b5a2f2a10
techGeekBoy <148032840+techGeekBoy@users.noreply.github.com>
2024-02-08 13:48:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let myArray = [];
-myArray.sort();
-let x = myArray.sort((a, b) => a - b);
-console.log(x);
-
-let update = x[myArray.length - 2];
-console.log(update);
+function secondHigest(arr) {
+  if (arr.length <= 1) {
+    return -Infinity;
+  }
+  let allsame = arr.every(function (value, index, array) {
+    return value === array[0];
+  });
+  if (allsame) {
+    return -Infinity;
+  }
+  let x = arr.sort((a, b) => a - b);
+  let update = x[arr.length - 2];
+  return update;
+}
